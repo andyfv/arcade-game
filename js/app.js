5ac4976d0f2e646ab97ffeff0f985c01c88d5da3
Andy <andyfilchev@gmail.com>
2018-05-21 21:28:13 +0300

    feat: Implement Enemy class

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(locationY) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.x = -100;
+    this.y = 60 + (85 * (locationY - 1));
+    this.speed;
+    this.randomSpeed();
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -14,6 +18,9 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if(this.checkBoundary()){
+        this.x += this.speed * dt;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,10 +28,70 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+Enemy.prototype.randomSpeed = function() {
+    min = Math.ceil(100);
+    max = Math.floor(400);
+    this.speed = Math.floor(Math.random() * (max - min) + min);
+    //this.speed = Math.floor((Math.random()*9));
+}
+
+Enemy.prototype.checkBoundary = function() {
+    if(this.x > 505){
+        this.resetPosition();
+        this.randomSpeed();
+        return false;
+    } else {
+        return true;
+    }
+}
+
+Enemy.prototype.resetPosition = function() {
+    this.randomPosition();
+}
+
+Enemy.prototype.randomPosition = function() {
+    min = Math.ceil(-500);
+    max = Math.floor(-100);
+    this.x = Math.floor(Math.random() * (max - min) + min);
+}
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+    constructor() {
+        this.x;
+        this.y;
+        this.sprite = 'images/char-boy.png';
+    }
 
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput(keyCode){
+        switch(keyCode){
+            case 37:
+                break;
+            case 38:
+                break;
+            case 39: 
+                break;
+            case 40: 
+                break;
+            default: 
+                break;
+        }
+    }
+
+    checkBoundary(){
+        if (this.x > 101 && this.x < (505 - 101)) {
+
+        }
+    }
+
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -42,5 +109,11 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    player.handleInput(allowedKeys[e.which]);
 });
+
+let allEnemies;
+let enemy1 = new Enemy(1);
+let enemy2 = new Enemy(2);
+let enemy3 = new Enemy(3);
+allEnemies = [enemy1, enemy2, enemy3];
